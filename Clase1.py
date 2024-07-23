6be2866ffbde96332971a8a335e047d27cd9deb5
Alextreme-Code <alexnoobfeliz9191soyalex@gmail.com>
2024-07-22 19:03:44 -0600

    Mi primer archivo subido
    22 Julio 2024

---
+++ Clase1.py
@@ -0,0 +1,217 @@
+print("Cartelera de cine")
+print("CINEMEX")
+print("1 - Abigail")
+print("2 - Godzilla Y Kong: El Nuevo Imperio")
+print("3 - Guerra Civil")
+print("4 - La primera profecia")
+opcion=int(input("Escoge una opción"))
+if opcion==1:
+    print("Usted selecciono la pelicula Abigail")
+    boletos=int(input("¿Cuantos boletos desea comprar?"))
+    precio=80
+    print("Entradas: ", boletos)
+    total=precio*boletos
+    print("Total: $", total)
+    print("Elegir fila de asiento")
+    print("Fila 1")
+    print("Fila 2")
+    print("Fila 3")
+    print("Fila 4")
+    fila=int(input("Escoga la fila de asiento"))
+    fila1=8
+    fila2=10
+    fila3=7
+    fila4=0
+    print("asientos disponibles:")
+    print("fila 1", ":", fila1, "asientos disponibles")
+    print("fila 2", ":", fila2, "asientos disponibles")
+    print("fila 3", ":", fila3, "asientos disponibles")
+    print("fila 4", ":", fila4, "asientos disponibles")
+    opcionAsiento=int(input("Seleccione la fila de asientos"))
+    if opcionAsiento==1:
+        print("selecciono 2 asientos de la fila 1")
+    if opcionAsiento==2:
+        print("selecciono 2 asientos de la fila 2")
+    if opcionAsiento==3:
+        print("selecciono 2 asientos de la fila 3")
+    if opcionAsiento==4:
+        print("selecciono 2 asientos de la fila 4")
+    masboletos=input("¿Desea escoger mas asientos? SI/NO")
+    if masboletos=="SI":
+       print("asientos disponibles:")
+    print("fila 1", ":", fila1, "asientos disponibles")
+    print("fila 2", ":", fila2, "asientos disponibles")
+    print("fila 3", ":", fila3, "asientos disponibles")
+    print("fila 4", ":", fila4, "asientos disponibles")
+    boletosrestantes=int(input("Complete sus entradas con otra fila"))
+    if boletosrestantes==1:
+        print("selecciono dos asientos mas")
+        print("Total a pagar", total)
+    if boletosrestantes==2:
+        print("No hay asientos disponibles")
+        print("Total a pagar", total)
+    if boletosrestantes==3:
+        print("Selecciono 3 asientos mas")
+        print("Total a pagar", total)
+    if boletosrestantes==4:
+        print("No hay asientos disponibles")
+        print("Total a pagar", total)
+
+    
+
+if opcion==2:
+    print("Usted selecciono la pelicula Godzilla Y Kong: El Nuevo Imperio")
+    boletos=int(input("¿Cuantos boletos desea comprar?"))
+    precio=80
+    print("Entradas: ", boletos)
+    total=precio*boletos
+    print("Total: $", total)
+    print("Elegir fila de asiento")
+    print("Fila 1")
+    print("Fila 2")
+    print("Fila 3")
+    print("Fila 4")
+    fila=int(input("Escoga la fila de asiento"))
+    fila1=8
+    fila2=10
+    fila3=7
+    fila4=0
+    print("asientos disponibles:")
+    print("fila 1", ":", fila1, "asientos disponibles")
+    print("fila 2", ":", fila2, "asientos disponibles")
+    print("fila 3", ":", fila3, "asientos disponibles")
+    print("fila 4", ":", fila4, "asientos disponibles")
+    opcionAsiento=int(input("Seleccione la fila de asientos"))
+    if opcionAsiento==1:
+        print("selecciono 2 asientos de la fila 1")
+    if opcionAsiento==2:
+        print("selecciono 2 asientos de la fila 2")
+    if opcionAsiento==3:
+        print("selecciono 2 asientos de la fila 3")
+    if opcionAsiento==4:
+        print("selecciono 2 asientos de la fila 4")
+    if masboletos=="SI":
+      print("asientos disponibles:")
+    print("fila 1", ":", fila1, "asientos disponibles")
+    print("fila 2", ":", fila2, "asientos disponibles")
+    print("fila 3", ":", fila3, "asientos disponibles")
+    print("fila 4", ":", fila4, "asientos disponibles")
+    boletosrestantes=int(input("Complete sus entradas con otra fila"))
+    if boletosrestantes==1:
+        print("selecciono dos asientos mas")
+        print("Total a pagar", total)
+    if boletosrestantes==2:
+        print("No hay asientos disponibles")
+        print("Total a pagar", total)
+    if boletosrestantes==3:
+        print("Selecciono 3 asientos mas")
+        print("Total a pagar", total)
+    if boletosrestantes==4:
+        print("No hay asientos disponibles")
+        print("Total a pagar", total)
+
+
+    
+if opcion==3:
+    print("Usted selecciono la pelicula Guerra Civil")
+    boletos=int(input("¿Cuantos boletos desea comprar?"))
+    precio=80
+    print("Entradas: ", boletos)
+    total=precio*boletos
+    print("Total: $", total)
+    print("Elegir fila de asiento")
+    print("Fila 1")
+    print("Fila 2")
+    print("Fila 3")
+    print("Fila 4")
+    fila=int(input("Escoga la fila de asiento"))
+    fila1=8
+    fila2=10
+    fila3=7
+    fila4=0
+    print("asientos disponibles:")
+    print("fila 1", ":", fila1, "asientos disponibles")
+    print("fila 2", ":", fila2, "asientos disponibles")
+    print("fila 3", ":", fila3, "asientos disponibles")
+    print("fila 4", ":", fila4, "asientos disponibles")
+    opcionAsiento=int(input("Seleccione la fila de asientos"))
+    if opcionAsiento==1:
+        print("selecciono 2 asientos de la fila 1")
+    if opcionAsiento==2:
+        print("selecciono 2 asientos de la fila 2")
+    if opcionAsiento==3:
+        print("selecciono 2 asientos de la fila 3")
+    if opcionAsiento==4:
+        print("selecciono 2 asientos de la fila 4")
+    if masboletos=="SI":
+       print("asientos disponibles:")
+    print("fila 1", ":", fila1, "asientos disponibles")
+    print("fila 2", ":", fila2, "asientos disponibles")
+    print("fila 3", ":", fila3, "asientos disponibles")
+    print("fila 4", ":", fila4, "asientos disponibles")
+    boletosrestantes=int(input("Complete sus entradas con otra fila"))
+    if boletosrestantes==1:
+        print("selecciono dos asientos mas")
+        print("Total a pagar", total)
+    if boletosrestantes==2:
+        print("No hay asientos disponibles")
+        print("Total a pagar", total)
+    if boletosrestantes==3:
+        print("Selecciono 3 asientos mas")
+        print("Total a pagar", total)
+    if boletosrestantes==4:
+        print("No hay asientos disponibles")
+        print("Total a pagar", total)
+
+    
+if opcion==4:
+    print("Usted selecciono la pelicula La primera profecia")
+    boletos=int(input("¿Cuantos boletos desea comprar?"))
+    precio=80
+    print("Entradas: ", boletos)
+    total=precio*boletos
+    print("Total: $", total)
+    print("Elegir fila de asiento")
+    print("Fila 1")
+    print("Fila 2")
+    print("Fila 3")
+    print("Fila 4")
+    fila=int(input("Escoga la fila de asiento"))
+    fila1=8
+    fila2=10
+    fila3=7
+    fila4=0
+    print("asientos disponibles:")
+    print("fila 1", ":", fila1, "asientos disponibles")
+    print("fila 2", ":", fila2, "asientos disponibles")
+    print("fila 3", ":", fila3, "asientos disponibles")
+    print("fila 4", ":", fila4, "asientos disponibles")
+    opcionAsiento=int(input("Seleccione la fila de asientos"))
+    if opcionAsiento==1:
+        print("selecciono 2 asientos de la fila 1")
+    if opcionAsiento==2:
+        print("selecciono 2 asientos de la fila 2")
+    if opcionAsiento==3:
+        print("selecciono 2 asientos de la fila 3")
+    if opcionAsiento==4:
+        print("selecciono 2 asientos de la fila 4")
+
+    if masboletos=="SI":
+     print("asientos disponibles:")
+    print("fila 1", ":", fila1, "asientos disponibles")
+    print("fila 2", ":", fila2, "asientos disponibles")
+    print("fila 3", ":", fila3, "asientos disponibles")
+    print("fila 4", ":", fila4, "asientos disponibles")
+    boletosrestantes=int(input("Complete sus entradas con otra fila"))
+    if boletosrestantes==1:
+        print("selecciono dos asientos mas")
+        print("Total a pagar", total)
+    if boletosrestantes==2:
+        print("No hay asientos disponibles")
+        print("Total a pagar", total)
+    if boletosrestantes==3:
+        print("Selecciono 3 asientos mas")
+        print("Total a pagar", total)
+    if boletosrestantes==4:
+        print("No hay asientos disponibles")
+        print("Total a pagar", total)
